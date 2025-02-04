6fbef93c95dd6ef07ceb6e1355f678f332104429
Черных Владислав <V.CHernykh@1nhtk.local>
2025-02-04 15:27:58 +0700

    a

--- README.md
+++
@@ -1,1 +0,0 @@
-# Vladchernykh.github.io
--- script.js
+++
@@ -1,7 +0,0 @@
-var images = ["url(fone.jpg)", "url(boom.jpg)"]; // список путей к фоновым картинкам
-var currentIndex = 0;
-
-function changeBackgroundImage() {
-    document.body.style.backgroundImage = images[currentIndex];
-    currentIndex = (currentIndex + 1) % images.length; // переход к следующей картинке
-}
